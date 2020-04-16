b9599408a2f355219050c76dba5df3d71e053ad1
闫磊 <yanlei@idss-cn.com>
2020-04-16 18:36:55 +0800

    添加查询接口

--- model/Movie.js
+++ model/Movie.js
@@ -4,16 +4,46 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 //创建约束实例
 const MovieSchema = new Schema({
-  keyword: String,              // 查询关键字
-  content: String,              // 查询内容
-  searchType: Number,           // 匹配类型  0: 模糊匹配  1: 完全匹配
-  movieType: String,            // 电影类型
-  desc: String,                 // 简介
-  actor: String,                // 主演
-  createTime: String,           // 创建时间, 时间戳
-  updateTime: String,           // 更新时间, 时间戳
-  movieUpdateTime: String       // 电影更新时间
-})
+  keyword: {                    // 查询关键字 【必选】
+    type: String,
+    required: true
+  },              
+  content: {                    // 查询内容 【必选】
+    type: String,
+    required: true
+  },              
+  matchType: {                  // 匹配类型  0: 模糊匹配  1: 完全匹配 【必选】
+    type: Number,
+    required: true
+  },
+  movieType: {                  // 电影类型 【必选】
+    type: Number,
+    required: true
+  },
+  movieUpdateTime: {            // 电影更新时间
+    type: [Number],
+    required: true
+  },
+  isUpdate: {
+    type: Number,
+    required: true,
+    default: 0
+  },
+  isValid: {
+    type: Number,
+    required: true,
+    default: 1
+  },
+  desc: {                       // 简介 【非必选】
+    type: String
+  },
+  actor: {                      // 主演 【非必选】
+    type: String
+  },
+  hotNum: Number                   // 热度 根据点击增加热度
+  // createTime: String,           // 创建时间, 时间戳
+  // updateTime: String,           // 更新时间, 时间戳
+}, { timestamps: { createdAt: 'createTime', updatedAt: 'updateTime' } })
 //创建模型对象
 const Movie = mongoose.model('Movie', MovieSchema)
 module.exports = Movie
--- router/index.js
+++ router/index.js
@@ -1,27 +1,112 @@
 //引入koa-router
 const router = require('koa-router')()
 //引入Movie Model
-const Movie = require('../model/Movie')
+const Movie = require('../model/Movie.js')
 
 // 接口路径
 const API_URL_DICT = {
   MOVIE_MANAGE: {
-    add: '/movie-manage'
+    add: '/movie-manage',
+    put: '/movie-manage',
+    list: '/movie-manage/list'
   }
 }
 
-// 电影管理相关接口
+// 新增数据
 router.post(API_URL_DICT.MOVIE_MANAGE.add, async (ctx, next) => {
   const resParams = ctx.request.body
-  console.log(ctx.request.body)
+  // TODO: 如果关键字已存在, 提示失败
   try {
     // 向数据库中插入数据
-    Artical.create(resParams)
+    Movie.create(resParams)
     console.log('插入数据成功')
+    // 响应结果
+    ctx.response.body = {
+      status: 'success',
+      message: '新增成功！',
+      content: {}
+    }
   } catch (error) {
     console.log('插入数据失败', error)
+    ctx.response.body = {
+      status: 'error',
+      message: '新增失败！',
+      content: {}
+    }
   }
-  ctx.response.body = 'success'
 })
+
+// 修改数据
+router.put(API_URL_DICT.MOVIE_MANAGE.put, async (ctx, next) => {
+  const resParams = ctx.request.body
+  const id = resParams.id
+  
+  Movie.findByIdAndUpdate(id, resParams, {}, function (err, doc) {
+    if (err) {
+      console.log('修改失败')
+      return false
+    }
+    // 响应结果
+    ctx.response.body = {
+      status: 'success',
+      message: '修改成功！',
+      content: Movie.findById(id)
+    }
+  })
+
+})
+
+// 查找数据
+router.post(API_URL_DICT.MOVIE_MANAGE.list, async (ctx, next) => {
+  const resParams = ctx.request.body
+  // 需要模糊查询的字段
+  // 关键字
+  const keyword = resParams.keyword
+  const keywordReg = new RegExp(keyword, 'i')
+
+  // 主演
+  const actor = resParams.actor
+  const actorReg = new RegExp(actor, 'i')
+
+  let searchParams = {
+    keyword: keywordReg,
+    actor: actorReg,
+    // isUpdate: resParams.isUpdate,
+    // isValid: resParams.isValid,
+    movieType: { $in: resParams.movieType },
+    movieUpdateTime: { 
+      $elemMatch: {
+        $in: resParams.movieUpdateTime
+      }
+    }
+  }
+
+  // 兼容movieType空数组情况
+  if (resParams.movieType.length <= 0) {
+    delete searchParams.movieType
+  }
+
+  if (resParams.movieUpdateTime.length <= 0) {
+    delete searchParams.movieUpdateTime
+  }
+
+  const content = await Movie.find(searchParams)
+
+  console.log('等待content', content)
+  // 响应结果
+  ctx.response.body = {
+    status: 'success',
+    message: '查询成功！',
+    content: {
+      total: content.length,
+      data: content
+    }
+  }
+})
+
+router.get('/xxx', async (ctx, next) => {
+  console.log(ctx)
+})
+
 
 module.exports = router
